5b1c40bfa4abc88dccc7ce5e5d8f939306c297fc
Simran <Simran.Mangukiya95@gmail.com>
2023-03-20 10:16:48 +0100

    add 2 projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,41 +1,46 @@
-import one from '../assets/svg/projects/hotelbooking.png'
-import two from '../assets/svg/projects/hackerNews.png'
-// import three from '../assets/svg/projects/three.svg'
-
-
-
+import one from '../assets/svg/projects/adminDashboardTemplate.png'
+import two from '../assets/svg/projects/ecommerceTemplate.png'
+import three from '../assets/svg/projects/hotelbooking.png'
+import four from '../assets/svg/projects/hackerNews.png'
 export const projectsData = [
     {
         id: 1,
+        projectName: 'Admin & Dashboard UI Template',
+        projectDesc: 'Erstellt Admin & Dashboard UI Template mit Bootstrap.',
+        tags: ['Html', 'Bootstrap 5', 'Javascript'],
+        code: 'https://github.com/SimaranM/bootstrap_javascript-UI',
+        demo: 'https://admin-dashboard-ui-template.netlify.app/',
+        image: one
+    },
+    {
+        id: 2,
+        projectName: 'E-commerce UI template',
+        projectDesc: 'Eine UI Template für eine Website zum Online-Einkauf von Lebensmitteln erstellt.',
+        tags: ['Html', 'css', 'Javascript'],
+        code: 'https://github.com/SimaranM/E-commerce-UI-template',
+        demo: 'https://ecommerce-ui-template.netlify.app/',
+        image: two
+    },
+    {
+        id: 3,
         projectName: 'Hotel Booking',
         projectDesc: 'Bei diesem Projekt handelt es sich um ein Hotelbuchungssystem, das es den Benutzern ermöglicht, ein Hotelzimmer, ein Appartmentzimmer und ein Resort im Voraus zu buchen.',
         tags: ['React', 'node js', 'mongoDB', 'Express', 'Tailwind CSS'],
         code: 'https://github.com/SimaranM/hotel_booking',
         demo: 'https://hotel-booking-pa88.onrender.com',
-        image: one
+        image: three
     },
     {
-        id: 2,
+        id: 4,
         projectName: 'Hacker News',
         projectDesc: 'Ein Projekt zum Sammeln von Nachrichtenartikeln mit einer Suchfunktion von Hacker News mit Hilfe der Restful API.',
         tags: ['React', 'CSS', 'React pagination'],
         code: 'https://github.com/SimaranM/hacker-news',
         demo: 'https://fluffy-lolly-667a9a.netlify.app',
-        image: two
+        image: four
     },
-    // {
-    //     id: 3,
-    //     projectName: 'Weather App',
-    //     projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-    //     tags: ['Django', 'CSS', 'Material Ui'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: three
-    // },
-   
+
 ]
-
-
 // Do not remove any fields.
 // Leave it blank instead as shown below
 
