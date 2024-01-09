15d672767e441fc0d0723bacb412d27b089eee94
Simran <Simran.Mangukiya95@gmail.com>
2024-01-09 10:21:59 +0100

    update skill

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -7,28 +7,28 @@ export const experienceData = [
         endYear: '01/2024'
     },
     {
-        id: 1,
+        id: 2,
         company: 'Wilysion Technologies, Surat, Indien',
         jobtitle: 'Junior Web-Entwicklerin',
         startYear: '12/2021 ',
         endYear: '05/2022'
     },
     {
-        id: 2,
+        id: 3,
         company: 'Keypress IT Services, Surat, Indien',
         jobtitle: 'Praktikum in der Webentwicklung',
         startYear: '01/2020',
         endYear: '09/2020'
     },
     {
-        id: 3,
+        id: 4,
         company: 'Fab Export, Surat, Indien',
         jobtitle: 'Senior Grafikdesignerin',
         startYear: '11/2017',
         endYear: '12/2019'
     },
     {
-        id: 4,
+        id: 5,
         company: 'Italica Floor Tiles Ltd., Morbi, Indien',
         jobtitle: 'Grafikdesignerin',
         startYear: '11/2015',
