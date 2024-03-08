aa668eb306abd01e3fc45b263bb31d4067135caa
Simran <Simran.Mangukiya95@gmail.com>
2024-03-08 16:43:23 +0100

    Update gatsby project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,44 +1,56 @@
-import one from '../assets/svg/projects/adminDashboardTemplate.png'
-import two from '../assets/svg/projects/ecommerceTemplate.png'
-import three from '../assets/svg/projects/hotelbooking.png'
-import four from '../assets/svg/projects/hackerNews.png'
+import one from '../assets/svg/projects/Fitness.png'
+import two from '../assets/svg/projects/hotelbooking.png'
+import three from '../assets/svg/projects/ecommerceTemplate.png'
+import four from '../assets/svg/projects/adminDashboardTemplate.png'
+import five from '../assets/svg/projects/hackerNews.png'
 export const projectsData = [
+  
     {
         id: 1,
-        projectName: 'Admin & Dashboard UI Template',
-        projectDesc: 'Erstellt Admin & Dashboard UI Template mit Bootstrap.',
-        tags: ['Html', 'Bootstrap 5', 'Javascript'],
-        code: 'https://github.com/SimaranM/bootstrap_javascript-UI',
-        demo: 'https://admin-dashboard-ui-template.netlify.app/',
+        projectName: 'Fitness Club',
+        projectDesc: 'Designed and developed a modern fitness studio website using Gatsby, incorporating SCSS for styling and TypeScript for enhanced functionality. Implemented eslint and husky for code quality.',
+        tags: ['Gatsby', 'SCSS', 'Typscript', 'Bootstrap'],
+        code: 'https://github.com/SimaranM/fitness_club',
+        demo: 'https://gatsby-fitness-club.netlify.app/',
         image: one
     },
     {
         id: 2,
-        projectName: 'E-commerce UI Template',
-        projectDesc: 'Eine UI Template für eine Website zum Online-Einkauf von Lebensmitteln erstellt.',
-        tags: ['Html', 'css', 'Javascript'],
-        code: 'https://github.com/SimaranM/E-commerce-UI-template',
-        demo: 'https://ecommerce-ui-template.netlify.app/',
-        image: two
-    },
-    {
-        id: 3,
         projectName: 'Hotel Booking',
         projectDesc: 'Bei diesem Projekt handelt es sich um ein Hotelbuchungssystem, das es den Benutzern ermöglicht, ein Hotelzimmer, ein Appartmentzimmer und ein Resort im Voraus zu buchen.',
         tags: ['React', 'node js', 'mongoDB', 'Express', 'Tailwind CSS'],
         code: 'https://github.com/SimaranM/hotel_booking',
         demo: 'https://hotel-booking-pa88.onrender.com',
+        image: two
+    },
+    {
+        id: 3,
+        projectName: 'E-commerce UI Template',
+        projectDesc: 'Eine UI Template für eine Website zum Online-Einkauf von Lebensmitteln erstellt.',
+        tags: ['Html', 'css', 'Javascript'],
+        code: 'https://github.com/SimaranM/E-commerce-UI-template',
+        demo: 'https://ecommerce-ui-template.netlify.app/',
         image: three
     },
     {
         id: 4,
+        projectName: 'Admin & Dashboard UI Template',
+        projectDesc: 'Erstellt Admin & Dashboard UI Template mit Bootstrap.',
+        tags: ['Html', 'Bootstrap 5', 'Javascript'],
+        code: 'https://github.com/SimaranM/bootstrap_javascript-UI',
+        demo: 'https://admin-dashboard-ui-template.netlify.app/',
+        image: four
+    },
+    {
+        id: 5,
         projectName: 'Hacker News',
         projectDesc: 'Ein Projekt zum Sammeln von Nachrichtenartikeln mit einer Suchfunktion von Hacker News mit Hilfe der Restful API.',
         tags: ['React', 'CSS', 'React pagination'],
         code: 'https://github.com/SimaranM/hacker-news',
         demo: 'https://fluffy-lolly-667a9a.netlify.app',
-        image: four
+        image: five
     },
+ 
 
 ]
 // Do not remove any fields.
